c15fc53f137f311840994d28c7d211816e1924cb
Vladimir Kurguzov <kurguzov74@gmail.com>
2026-04-09 13:09:08 +0300

    логирование

--- projects/evo-lib/src/lib/evo/logging/logger.interface.ts
+++ projects/evo-lib/src/lib/evo/logging/logger.interface.ts
@@ -6,14 +6,14 @@ export type TColor = 'red' | 'green' | 'blue' | 'yellow' | 'magenta' | 'cyan' | 
 
 /** 2. Цвета для консоли - на основе TColor идет присвоение значения */
 export const colorStyles: Record<TColor, string> = {
-    red: 'background: #ff4444; color: #000000; padding: 2px 4px; border-radius: 3px',
-    green: 'background: #00ff00; color: #000000; padding: 2px 4px; border-radius: 3px',
-    blue: 'background: #3399ff; color: #ffffff; padding: 2px 4px; border-radius: 3px',
-    yellow: 'background: #ffcc00; color: #000000; padding: 2px 4px; border-radius: 3px',
-    magenta: 'background: #ff44ff; color: #000000; padding: 2px 4px; border-radius: 3px',
-    cyan: 'background: #00f0ff; color: #000000; padding: 2px 4px; border-radius: 3px',
-    white: 'background: #eeeeee; color: #000000; padding: 2px 4px; border-radius: 3px',
-    gray: 'background: #888888; color: #ffffff; padding: 2px 4px; border-radius: 3px'
+    red: 'background: #ff4444; color: #000000; padding: 0 5px; border-radius: 3px',
+    green: 'background: #00ff00; color: #000000; padding: 0 5px; border-radius: 3px',
+    blue: 'background: #3399ff; color: #ffffff; padding: 0 5px; border-radius: 3px',
+    yellow: 'background: #ffcc00; color: #000000; padding: 0 5px; border-radius: 3px',
+    magenta: 'background: #ff44ff; color: #000000; padding: 0 5px; border-radius: 3px',
+    cyan: 'background: #00f0ff; color: #000000; padding: 0 5px; border-radius: 3px',
+    white: 'background: #eeeeee; color: #000000; padding: 0 5px; border-radius: 3px',
+    gray: 'background: #888888; color: #ffffff; padding: 0 5px; border-radius: 3px'
 };
 
 /** Методы логирования */
