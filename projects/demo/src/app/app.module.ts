1b0275a1909fa87eb762f2bd7b83f721a0316fe3
Vladimir Kurguzov <kurguzov74@gmail.com>
2026-04-13 11:36:43 +0300

    LogAll

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
 import { EvoLibModule } from 'evo-lib';
 import { ChildComponentComponent } from './child-component/child-component.component';
-import { HttpClientModule } from '@angular/common/http';
-
 
 
 @NgModule({
