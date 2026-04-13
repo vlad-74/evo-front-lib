8cf33b43d52c4834e7c524b4e073d2f872f4a195
Vladimir Kurguzov <kurguzov74@gmail.com>
2026-04-13 18:51:18 +0300

    page

--- projects/evo-lib/src/lib/evo/page/page.service.ts
+++ projects/evo-lib/src/lib/evo/page/page.service.ts
@@ -1,3 +1,4 @@
+// evo-lib\src\lib\evo\page\page.service.ts
 import { ComponentRef, Injectable, ViewContainerRef} from '@angular/core';
 import { ResolverProviderService } from './resolver-provider.service';
 
@@ -34,61 +35,100 @@ export class PageService {
     public send(config: ICreatePage): void {
         const cfr = this.resolverProvider?.getComponentFactoryResolver();
         if (!cfr) {
-            throw new Error('ComponentFactoryResolver недоступен. Проверьте инициализацию EvoLibModule.');
+            throw new Error('ComponentFactoryResolver недоступен.');
         }
 
-        const { component, viewContainerRef, isMultiPage, inputs, outputs } = config;
+        const { component, viewContainerRef: vcr, isMultiPage, inputs, outputs } = config;
 
-        if (!viewContainerRef || typeof viewContainerRef.createEmbeddedView !== 'function') {
+        if (!vcr?.createComponent) {
             console.error('Invalid ViewContainerRef provided');
             return;
         }
 
-        const vcr = viewContainerRef;
-
-        // Инициализируем массив ссылок
         if (!this.componentRefs.has(vcr)) {
             this.componentRefs.set(vcr, []);
         }
 
-        // Удаляем предыдущие компоненты, если isMultiPage === false
         if (!isMultiPage) {
             this.clearContainer(vcr);
         }
 
-        if (!cfr) {
-            console.error('ComponentFactoryResolver недоступен. Убедитесь, что EvoLib инициализирован.');
+        const componentRef = vcr.createComponent(
+            cfr.resolveComponentFactory(component)
+        );
+
+        // 👉 проброс служебных ссылок - нужно указать в компоненте
+        // private __componentRef!: ComponentRef<any>;
+        // private __vcr!: ViewContainerRef;
+        Object.assign(componentRef.instance as any, {
+            __componentRef: componentRef,
+            __vcr: vcr
+        });
+
+        // 👉 сохраняем ссылку
+        // tslint:disable-next-line:no-non-null-assertion
+        this.componentRefs.get(vcr)!.push(componentRef);
+
+        // 👉 inputs
+        if (inputs) {
+            Object.assign(componentRef.instance, inputs);
+        }
+
+        // 👉 outputs
+        if (outputs) {
+            for (const [key, handler] of Object.entries(outputs)) {
+                const emitter = (componentRef.instance as any)[key];
+                if (emitter?.subscribe) {
+                    const sub = emitter.subscribe(handler);
+                    componentRef.onDestroy(() => sub.unsubscribe());
+                }
+            }
+        }
+
+        evo.log.colorWarn(
+            'white',
+            'createPage',
+            'common',
+            'В PageService - создан компонент ',
+            component.name
+        );
+    }
+
+    /**
+     * Удаляет конкретный компонент из контейнера
+     * Пример использования в компоненте
+     * private __componentRef!: ComponentRef<any>;
+     * private __vcr!: ViewContainerRef;
+     *
+     * close(): void {
+     *     this.pageService.removeComponent(this.__vcr, this.__componentRef);
+     * }
+     */
+    public removeComponent(
+        vcr: ViewContainerRef,
+        componentRef: ComponentRef<any>
+    ): void {
+        const refs = this.componentRefs.get(vcr);
+
+        if (!refs || refs.length === 0) {
             return;
         }
 
-        const factory = cfr.resolveComponentFactory(component);
-        const componentRef = vcr.createComponent(factory);
+        const index = refs.indexOf(componentRef);
 
-        // Сохраняем ссылку
-        // tslint:disable-next-line:no-non-null-assertion
-        const refs = this.componentRefs.get(vcr)!;
-        refs.push(componentRef);
-
-        // Передаём @Input()
-        if (inputs) {
-            Object.keys(inputs).forEach(key => {
-                (componentRef.instance as any)[key] = inputs[key];
-            });
+        if (index === -1) {
+            console.warn('ComponentRef не найден в контейнере');
+            return;
         }
 
-        // Подписываемся на @Output()
-        if (outputs) {
-            Object.keys(outputs).forEach(outputName => {
-                const emitter = (componentRef.instance as any)[outputName];
-                const handler = outputs[outputName];
+        // Удаляем из ViewContainerRef
+        vcr.remove(vcr.indexOf(componentRef.hostView));
 
-                if (emitter && typeof emitter.subscribe === 'function') {
-                    const subscription = emitter.subscribe(handler);
-                    componentRef.onDestroy(() => subscription.unsubscribe());
-                }
-            });
-        }
-        evo.log.colorWarn('white', 'createPage', 'common', 'В PageService - создан компонент ', component.name);
+        // Уничтожаем компонент
+        componentRef.destroy();
+
+        // Удаляем из массива
+        refs.splice(index, 1);
     }
 
     /**
